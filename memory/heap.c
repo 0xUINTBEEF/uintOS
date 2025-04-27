202911a2c9406bb882b520a8c7800b04689f7f35
0xUINTBEEF <35432275+0xUINTBEEF@users.noreply.github.com>
2025-04-27 17:31:04 +0300

    Update heap.c

--- memory/heap.c
+++ memory/heap.c
@@ -250,16 +250,15 @@ void free(void *ptr) {
 static void mem_copy(void *dest, const void *src, size_t n) {
     uint8_t *d = (uint8_t*)dest;
     const uint8_t *s = (const uint8_t*)src;
-    for (size_t i = 0; i < n; i++) {
-        d[i] = s[i];
+    while (n--) {
+        *d++ = *s++;
     }
 }
 
-// Memory set helper function
 static void mem_set(void *dest, int val, size_t n) {
     uint8_t *d = (uint8_t*)dest;
-    for (size_t i = 0; i < n; i++) {
-        d[i] = (uint8_t)val;
+    while (n--) {
+        *d++ = (uint8_t)val;
     }
 }
 
